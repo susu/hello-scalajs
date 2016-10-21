c42ffefbb20dcb09aed042d31d4c80a8d1bc8804
Marton Suranyi <marton.suranyi@balabit.com>
2016-10-21 20:09:32 +0200

    first interactive scalajs app

---
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -0,0 +1,26 @@
+
+package tutorial.webapp
+
+import scala.scalajs.js.JSApp
+import org.scalajs.dom
+import dom.document
+
+import scala.scalajs.js.annotation.JSExport
+
+object TutorialApp extends JSApp {
+  def main(): Unit = {
+    appendPar(document.body, "Hello Scala.js!")
+  }
+
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
+  }
+
+  @JSExport
+  def addClickedMessage(): Unit = {
+    appendPar(document.body, "You clicked the button!")
+  }
+}
