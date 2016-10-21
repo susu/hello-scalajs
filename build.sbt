6d2a2130d7ea916058976000ce769ee7a297557b
Marton Suranyi <marton.suranyi@balabit.com>
2016-10-21 21:19:40 +0200

    replace DOM manipulation with jquery and add js bundling

--- build.sbt
+++ build.sbt
@@ -4,4 +4,14 @@ name := "Hello Scala.js"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.1"
+// libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.1"
+libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.9.0"
+
+// Enabling JS bundling
+skip in packageJSDependencies := false
+jsDependencies += "org.webjars" % "jquery" % "2.1.4" / "2.1.4/jquery.js"
+
+// Testing
+jsDependencies += RuntimeDOM
+libraryDependencies += "com.lihaoyi" %%% "utest" % "0.4.3" % "test"
+testFrameworks += new TestFramework("utest.runner.Framework")
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -2,25 +2,19 @@
 package tutorial.webapp
 
 import scala.scalajs.js.JSApp
-import org.scalajs.dom
-import dom.document
-
-import scala.scalajs.js.annotation.JSExport
+import org.scalajs.jquery.jQuery
 
 object TutorialApp extends JSApp {
   def main(): Unit = {
-    appendPar(document.body, "Hello Scala.js!")
+    jQuery(setupUI _)
   }
 
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
+  def setupUI(): Unit = {
+    jQuery("#click-me-button").click(addClickedMessage _)
+    jQuery("body").append("<p>Hello Scala.js!</p>")
   }
 
-  @JSExport
   def addClickedMessage(): Unit = {
-    appendPar(document.body, "You clicked the button!")
+    jQuery("body").append("<p>You clicked the button!</p>")
   }
 }
